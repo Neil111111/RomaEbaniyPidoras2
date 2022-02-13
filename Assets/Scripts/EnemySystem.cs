a5b4c66b3d2a1e87b24b10d3551611963291fec9
Neil111111 <danilshevchuk89@gmail.com>
2022-02-13 15:26:04 +0700

    Добавил врагов

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -5,7 +5,35 @@ using UnityEngine;
 public class EnemySystem : MonoBehaviour
 {
     public float enemyHealth;
+    public float speed;
+    /*public float stopDis;
+    public float retreatDis;*/
+
+    public Transform player;
     public PlayerAttack _strengthAttack;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;   
+    }
+
+    void Update() 
+    {   
+        //Shooting Enemy
+        /*if(Vector2.Distance(transform.position,player.position) > stopDis)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,player.position,speed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position,player.position) < stopDis && Vector2.Distance(transform.position,player.position) > retreatDis){
+            transform.position = this.transform.position;
+        }
+        else if(Vector2.Distance(transform.position, player.position) < retreatDis){
+            transform.position = Vector2.MoveTowards(transform.position,player.position, -speed * Time.deltaTime);
+        }*/
+
+        //Standart enemy
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
 
     public void TakeHit(float damage)
     {
